de2295bac53b44f218313e2ba4f3785d2a622155
nguyenthuongquan <nguyenthuongquan@gmail.com>
2021-09-21 15:09:56 +0700

    Implement api to delete item

--- Catalog/Repositories/InMemItemsRepository.cs
+++ Catalog/Repositories/InMemItemsRepository.cs
@@ -35,5 +35,11 @@ namespace Catalog.Repositories {
             items[index] = item;
 
         }
+
+        public void DeleteItem(Guid id)
+        {
+            var index = items.FindIndex(existingItem => existingItem.Id == id);
+            items.RemoveAt(index);
+        }
     }
 }
