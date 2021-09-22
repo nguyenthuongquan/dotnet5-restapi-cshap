54fb2d3e99bba28fbe8da87402299291ef3752bb
nguyenthuongquan <nguyenthuongquan@gmail.com>
2021-09-22 18:02:45 +0700

    Enabling authentication in MongoDB

--- Catalog/Settings/MongoDbSettings.cs
+++ Catalog/Settings/MongoDbSettings.cs
@@ -6,11 +6,16 @@ namespace Catalog.Settings
 
         public int Port {get; set; }
 
+        public string User {get; set; } 
+
+        public string Password {get; set; } 
+
         public string ConnectionString 
         {
             get
             {
-                return $"mongodb://{Host}:{Port}";
+                // return $"mongodb://{Host}:{Port}"; //no authentication
+                return $"mongodb://{User}:{Password}@{Host}:{Port}";
             }
 
         }
