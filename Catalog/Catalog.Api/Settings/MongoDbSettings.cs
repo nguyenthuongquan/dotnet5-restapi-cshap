1ea58a123785424dc743eb2e37fb4f94852c639a
nguyenthuongquan <nguyenthuongquan@gmail.com>
2021-09-28 17:35:21 +0700

    Refactor framework for Unit Testing

---
+++ Catalog/Catalog.Api/Settings/MongoDbSettings.cs
@@ -0,0 +1,25 @@
+namespace Catalog.Api.Settings
+{
+    public class MongoDbSettings
+    {
+        public string Host {get; set; }
+
+        public int Port {get; set; }
+
+        public string User {get; set; } 
+
+        public string Password {get; set; } 
+
+        public string ConnectionString 
+        {
+            get
+            {
+                // return $"mongodb://{Host}:{Port}"; //no authentication
+                return $"mongodb://{User}:{Password}@{Host}:{Port}";
+            }
+
+        }
+
+    }
+    
+}
